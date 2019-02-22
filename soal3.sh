efbc55a0cfa8119da1299c4c0279a0e88940478f
bntng02tc17 <37019764+bntng02tc17@users.noreply.github.com>
2019-02-22 13:21:10 +0700

    perbaikan

--- soal3.sh
+++ soal3.sh
@@ -5,9 +5,15 @@ g_pass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 name="password"
 num=1
 
-while test -e "$name$num.txt"; do
-num=$((num+1))
+#while test -e "$name$num.txt"; do
+#num=$((num+1))
+#done
+
+while [ -f "$name$num.txt" ]
+do
+ num=$((num+1))
 done
 
-fname= "$name$num.txt"
-echo "$g_pass" > $fname
+`touch "$name$num.txt"`
+fname="$name$num.txt"
+echo "$num$g_pass" > $fname
